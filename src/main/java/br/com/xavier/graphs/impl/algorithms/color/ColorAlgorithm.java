68c3cee39347f4d7287f93f5de3b4c477f3bfcab
Matheus Arleson Sales Xavier <matheusarleson@gmail.com>
2016-05-29 19:42:08 -0300

    Fix coloring algorithm

--- src/main/java/br/com/xavier/graphs/impl/algorithms/color/ColorAlgorithm.java
+++ src/main/java/br/com/xavier/graphs/impl/algorithms/color/ColorAlgorithm.java
@@ -1,6 +1,8 @@
 package br.com.xavier.graphs.impl.algorithms.color;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -13,7 +15,7 @@ import br.com.xavier.graphs.abstractions.edges.AbstractWeightedEdge;
 import br.com.xavier.graphs.abstractions.nodes.AbstractColoredNode;
 import br.com.xavier.graphs.impl.algorithms.degree.DegreeSortAlgorithm;
 import br.com.xavier.graphs.impl.edges.DefaultUnweightedEdge;
-import br.com.xavier.graphs.impl.nodes.ColoredNode;
+import br.com.xavier.graphs.impl.nodes.Room;
 import br.com.xavier.graphs.impl.simple.undirected.DefaultSUUGraph;
 import br.com.xavier.graphs.interfaces.Graph;
 import br.com.xavier.graphs.util.Util;
@@ -24,60 +26,40 @@ public class ColorAlgorithm {
 	public ColorAlgorithm() {}
 	
 	public static void main(String[] args) {
-		DefaultSUUGraph<ColoredNode, DefaultUnweightedEdge<ColoredNode>> graph = new DefaultSUUGraph<>();
+		DefaultSUUGraph<Room, DefaultUnweightedEdge<Room>> graph = new DefaultSUUGraph<>();
 		
-		ColoredNode cn1 = new ColoredNode();
-//		ColoredNode cn2 = new ColoredNode();
-//		ColoredNode cn3 = new ColoredNode();
-//		ColoredNode cn4 = new ColoredNode();
-//		ColoredNode cn5 = new ColoredNode();
-//		ColoredNode cn6 = new ColoredNode();
-//		ColoredNode cn7 = new ColoredNode();
-//		ColoredNode cn8 = new ColoredNode();
+		Room r1 = new Room("SALA1", 0, 0, 10, 20);
+		Room r2 = new Room("SALA2", 0, 20, 10, 20);
+		Room r3 = new Room("SALA3", 0, 40, 10, 20);
+		Room r4 = new Room("SALA4", 0, 60, 10, 20);
+		Room r5 = new Room("SALA5", 0, 80, 10, 20);
 		
-		graph.addNode(cn1);
-//		graph.addNode(cn2);
-//		graph.addNode(cn3);
-//		graph.addNode(cn4);
-//		graph.addNode(cn5);
-//		graph.addNode(cn6);
-//		graph.addNode(cn7);
-//		graph.addNode(cn8);
+		graph.addNode(r1);
+		graph.addNode(r2);
+		graph.addNode(r3);
+		graph.addNode(r4);
+		graph.addNode(r5);
 		
-//		DefaultUnweightedEdge<ColoredNode> edge12 = new DefaultUnweightedEdge<ColoredNode>(cn1, cn2);
-//		DefaultUnweightedEdge<ColoredNode> edge13 = new DefaultUnweightedEdge<ColoredNode>(cn1, cn3);
-//		DefaultUnweightedEdge<ColoredNode> edge23 = new DefaultUnweightedEdge<ColoredNode>(cn2, cn3);
-//		DefaultUnweightedEdge<ColoredNode> edge34 = new DefaultUnweightedEdge<ColoredNode>(cn3, cn4);
-//		DefaultUnweightedEdge<ColoredNode> edge35 = new DefaultUnweightedEdge<ColoredNode>(cn3, cn5);
-//		DefaultUnweightedEdge<ColoredNode> edge36 = new DefaultUnweightedEdge<ColoredNode>(cn3, cn6);
-//		DefaultUnweightedEdge<ColoredNode> edge45 = new DefaultUnweightedEdge<ColoredNode>(cn4, cn5);
-//		DefaultUnweightedEdge<ColoredNode> edge47 = new DefaultUnweightedEdge<ColoredNode>(cn4, cn7);
-//		DefaultUnweightedEdge<ColoredNode> edge56 = new DefaultUnweightedEdge<ColoredNode>(cn5, cn6);
-//		DefaultUnweightedEdge<ColoredNode> edge57 = new DefaultUnweightedEdge<ColoredNode>(cn5, cn7);
-//		DefaultUnweightedEdge<ColoredNode> edge58 = new DefaultUnweightedEdge<ColoredNode>(cn5, cn8);
-//		DefaultUnweightedEdge<ColoredNode> edge68 = new DefaultUnweightedEdge<ColoredNode>(cn6, cn8);
-//		DefaultUnweightedEdge<ColoredNode> edge78 = new DefaultUnweightedEdge<ColoredNode>(cn7, cn8);
-//		
-//		graph.addEdge(edge12);
-//		graph.addEdge(edge13);
-//		graph.addEdge(edge23);
-//		graph.addEdge(edge34);
-//		graph.addEdge(edge35);
-//		graph.addEdge(edge36);
-//		graph.addEdge(edge45);
-//		graph.addEdge(edge47);
-//		graph.addEdge(edge56);
-//		graph.addEdge(edge57);
-//		graph.addEdge(edge58);
-//		graph.addEdge(edge68);
-//		graph.addEdge(edge78);
+		DefaultUnweightedEdge<Room> edge12 = new DefaultUnweightedEdge<Room>(r1, r2);
+		DefaultUnweightedEdge<Room> edge13 = new DefaultUnweightedEdge<Room>(r1, r3);
+		DefaultUnweightedEdge<Room> edge23 = new DefaultUnweightedEdge<Room>(r2, r3);
+		DefaultUnweightedEdge<Room> edge34 = new DefaultUnweightedEdge<Room>(r3, r4);
+		DefaultUnweightedEdge<Room> edge35 = new DefaultUnweightedEdge<Room>(r3, r5);
+		DefaultUnweightedEdge<Room> edge45 = new DefaultUnweightedEdge<Room>(r4, r5);
+		
+		graph.addEdge(edge12);
+		graph.addEdge(edge13);
+		graph.addEdge(edge23);
+		graph.addEdge(edge34);
+		graph.addEdge(edge35);
+		graph.addEdge(edge45);
 		
 		ColorAlgorithm ca = new ColorAlgorithm();
 		ca.applyColor(graph);
 		
-		Set<ColoredNode> allNodes = graph.getAllNodes();
-		for (ColoredNode node : allNodes) {
-			System.out.println(node);
+		Set<Room> allNodes = graph.getAllNodes();
+		for (Room room : allNodes) {
+			System.out.println(room);
 		}
 	}
 	
@@ -105,15 +87,15 @@ public class ColorAlgorithm {
 		Set<CN> uncoloredNodesSet = new LinkedHashSet<>(graph.getAllNodes());
 		Set<CN> neighborNodeSet = new LinkedHashSet<>();
 		
-		
 		DegreeSortAlgorithm dsa = new DegreeSortAlgorithm();
 		Map<Integer, List<CN>> degreeMap = dsa.getDegreeMap(graph);
+		List<Integer> degreesList = new ArrayList<>(degreeMap.keySet());
+		Collections.sort(degreesList, Collections.reverseOrder());
 		
 		while(!uncoloredNodesSet.isEmpty()){
-			Set<Integer> degreeSet = degreeMap.keySet();
-			for (Integer degree : degreeSet) {
-				Color color = colorIterator.next();
-				
+			Color color = colorIterator.next();
+			
+			for (Integer degree : degreesList) {
 				List<CN> degreeNodesList = degreeMap.get(degree);
 				ListIterator<CN> degreeIterator = degreeNodesList.listIterator();
 				while(degreeIterator.hasNext()){
---
+++ src/main/java/br/com/xavier/graphs/impl/nodes/Room.java
@@ -0,0 +1,146 @@
+package br.com.xavier.graphs.impl.nodes;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.io.Serializable;
+
+import br.com.xavier.graphs.abstractions.nodes.AbstractColoredNode;
+import br.com.xavier.graphs.interfaces.nodes.ColoredNode;
+
+public class Room extends AbstractColoredNode implements ColoredNode, Serializable {
+	
+	private static final long serialVersionUID = 8282034103902232581L;
+	
+	//XXX PROPERTTIES
+	private final Rectangle roomRectangle;
+	private String alias;
+	private Color color;
+	
+	//XXX CONSTRUCTORS
+	public Room(String alias, int xPosition, int yPosition, int width, int height) {
+		this(alias, xPosition, yPosition, width, height, Color.WHITE);
+	}
+	
+	public Room(String alias, int xPosition, int yPosition, int width, int height, Color color) {
+		super();
+		this.alias = alias;
+		this.roomRectangle = new Rectangle(xPosition, yPosition, width, height);
+		this.color = color;
+	}
+	
+	//XXX OVERRIDE METHODS
+	@Override
+	public Color getColor() {
+		return color;
+	}
+
+	@Override
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((alias == null) ? 0 : alias.hashCode());
+		result = prime * result + ((roomRectangle == null) ? 0 : roomRectangle.hashCode());
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { return true; }
+		if (obj == null) { return false; }
+		if (getClass() != obj.getClass()) { return false; }
+		
+		Room other = (Room) obj;
+		if (alias == null) {
+			if (other.alias != null) { 
+				return false; 
+			}
+		} else if (!alias.equals(other.alias)) {
+			return false;
+		}
+		
+		if (roomRectangle == null) {
+			if (other.roomRectangle != null) { 
+				return false; 
+			}
+		} else if (!roomRectangle.equals(other.roomRectangle)) {
+			return false;
+		}
+		
+		if (color == null) {
+			if (other.color != null) { 
+				return false; 
+			}
+		} else if (!color.equals(other.color)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+//	@Override
+//	public String getLabel() {
+//		return getAlias();
+//	}
+	
+	@Override
+	public String toString() {
+		return "Room [" 
+			+ "alias=" + alias
+			+ ", roomRectangle= " + roomRectangle
+			+ ", color=" + color 
+		+ "]";
+	}
+
+	//XXX METHODS
+	public boolean intersects(Room otherRoom){
+		return this.roomRectangle.intersects(otherRoom.roomRectangle);
+	}
+	
+	public Room getExpandedTop(){
+		return new Room(alias, getX(), getY(), getWidth(), getHeight()+1);
+	}
+	
+	public Room getExpandedBottom(){
+		return new Room(alias, getX(), getY()-1, getWidth(), getHeight());
+	}
+	
+	public Room getExpandedLeft(){
+		return new Room(alias, getX()-1, getY(), getWidth(), getHeight());
+	}
+	
+	public Room getExpandedRight(){
+		return new Room(alias, getX(), getY(), getWidth()+1, getHeight());
+	}
+	
+	//XXX GETTERS/SETTERS
+	public String getAlias() {
+		return alias;
+	}
+	
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	
+	public int getX() {
+		return new Integer(roomRectangle.x);
+	}
+
+	public int getY() {
+		return new Integer(roomRectangle.y);
+	}
+
+	public int getWidth() {
+		return new Integer(roomRectangle.width);
+	}
+
+	public int getHeight() {
+		return new Integer(roomRectangle.height);
+	}
+
+}
